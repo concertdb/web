4fe35adc70a188c1a408e4c1c2edfc491cb7d2ff
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-05-31 16:37:50 -0700

    Remove unused boilerplate components. `hero, about, user, home`

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,11 +1,7 @@
 import angular from 'angular';
-import Home from './home/home';
-import About from './about/about';
 import Concert from './concert/concert';
 
 let componentModule = angular.module('app.components', [
-    Home.name,
-    About.name,
     Concert.name
 ]);
 
