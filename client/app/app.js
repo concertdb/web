fbacf4cfe85d78f0f8cd86004244d7147bc38570
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-05-31 16:40:09 -0700

    Remove `client/app/services` dir. Consolidate the Concert-related services into `app/component/concert`. Next commit will have working service files in new location.

--- client/app/app.js
+++ client/app/app.js
@@ -2,15 +2,13 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import Common from './common/common';
 import Components from './components/components';
-import Services from './services/services';
 import AppComponent from './app.component';
 import 'normalize.css';
 
 angular.module('app', [
     uiRouter,
     Common.name,
-    Components.name,
-    Services.name
+    Components.name
   ])
   .config(($locationProvider) => {
     "ngInject";
--- client/app/services/trackService/trackService.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import TrackService from './trackService.service';
-
-let trackServiceModule = angular.module('trackService', [])
-
-.service('TrackService', TrackService);
-
-export default trackServiceModule;
