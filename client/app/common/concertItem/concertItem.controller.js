23a6ae90c02a483eafae7dad759f8a84416ba18a
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-06-11 15:42:31 -0700

    Add PouchDB via CDN

--- client/app/common/concertItem/concertItem.controller.js
+++ client/app/common/concertItem/concertItem.controller.js
@@ -1,10 +1,33 @@
-function concertItemController($rootScope, $scope, $state, $stateParams, ConcertService){
+function concertItemController($log, $rootScope, $scope, $window, $state, $stateParams, ConcertService){
     "ngInject";
+
+    const concertId = $stateParams.id;
+    const db = $window.PouchDB('concertDbLocal');
     const vm = this;
+
     vm.name = 'concertItem';
-    vm.concertSongs = ConcertService.concertSongs[$stateParams.id];
-    vm.currentConcert = ConcertService.currentConcert[$stateParams.id];
-    vm.noConcertFound = (vm.concertSongs === undefined);
+
+    // GET concertAndPlaylist {} from PouchDb
+    db.get(concertId).then(function (concertAndPlaylist) {
+        let concert = concertAndPlaylist.concert;
+        let playlist = concertAndPlaylist.playlist;
+
+        //update service cache (maintain current functionality until refactor complete)
+        ConcertService.currentConcert[concert.concertId] = concert;
+        ConcertService.concertSongs[concert.concertId] = playlist;
+
+        //@todo: remove $scope.apply.
+        $scope.$apply(function() {
+            // Value has been bound within Angular's context, so a digest will be triggered and the DOM updated
+            vm.currentConcert = concert;
+            vm.concertSongs = playlist;
+            vm.noConcertFound = (vm.concertSongs === undefined);
+        });
+
+    }).catch(function (err) {
+        //@todo: Add archiveOrg search fn here. Also, refactor $stateParams to have necessary values for search.
+        $log.error(err);
+    });
 
     ////@todo: refactor component-to-component communication. $broadcast not in ng2.
     $rootScope
