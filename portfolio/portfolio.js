d9f68ff5ac4c171f201d7d9b9484ac6ca7c5aced
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-06-15 10:07:26 -0700

    Added links to project sites to portfolio

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -12,6 +12,7 @@ angular.module('portfolio')
         {
             title: 'ConcertDB',
             image: 'http://i.imgur.com/Ypl3MSb.jpg',
+            sitelink: 'http://matthewvilhauer.com/concertdb',
             githublink: 'https://github.com/concertdb/web',
             description: 'ConcertDB is my true passion project. It allows you to stream audio recordings of concerts from Archive.org and make custom playlists of songs from different concerts.',
             technologies: 'Node, Express, Angular, Angular-material, ES6, NG6-Starter-kit, PouchDB, Webpack, Babel, 3rd Party APIs, Component-driven Architecture'
@@ -19,6 +20,7 @@ angular.module('portfolio')
         {
             title: 'Nomad',
             image: 'http://i.imgur.com/26nsA7U.jpg',
+            sitelink: 'https://travel-nomad.herokuapp.com/',
             githublink: 'https://github.com/WDI28-Nomad/nomad',
             description: 'Nomad is a budgeting app that allows you to estimate your expenses for upcoming trips.',
             technologies: 'Ruby on Rails, PostgreSQL, Active Record, HTML/CSS, Rspec, JQuery, Highcharts, Google Maps API, Carrierwave, HTTParty'
@@ -26,6 +28,7 @@ angular.module('portfolio')
         {
             title: 'Concert Tracker',
             image: 'http://i.imgur.com/Fo9di1V.jpg',
+            sitelink: 'https://github.com/matthewvilhauer/concert-tracker',
             githublink: 'https://github.com/matthewvilhauer/concert-tracker',
             description: 'My first attempt at building a web app from the ground up. Concert Tracker allows you to create and save lists of your favorite concerts.',
             technologies: 'Node, Express, MongoDB, Handlebars, JQuery'
@@ -38,24 +41,24 @@ angular.module('portfolio')
       originatorEv = ev;
       $mdOpenMenu(ev);
     };
-    this.notificationsEnabled = true;
-    this.toggleNotifications = function() {
-      this.notificationsEnabled = !this.notificationsEnabled;
-    };
-    this.redial = function() {
-      $mdDialog.show(
-        $mdDialog.alert()
-          .targetEvent(originatorEv)
-          .clickOutsideToClose(true)
-          .parent('body')
-          .title('Suddenly, a redial')
-          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
-          .ok('That was easy')
-      );
-      originatorEv = null;
-    };
-    this.checkVoicemail = function() {
-      // This never happens.
-    };
+    // this.notificationsEnabled = true;
+    // this.toggleNotifications = function() {
+    //   this.notificationsEnabled = !this.notificationsEnabled;
+    // };
+    // this.redial = function() {
+    //   $mdDialog.show(
+    //     $mdDialog.alert()
+    //       .targetEvent(originatorEv)
+    //       .clickOutsideToClose(true)
+    //       .parent('body')
+    //       .title('Suddenly, a redial')
+    //       .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
+    //       .ok('That was easy')
+    //   );
+    //   originatorEv = null;
+    // };
+    // this.checkVoicemail = function() {
+    //   // This never happens.
+    // };
 
 });
