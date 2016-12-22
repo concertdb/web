66e111338d6d7fd263fcdd9ea4fbb6542975a601
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-12-22 09:19:46 -0800

    Testing example file.

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use('/portfolio', portfolioPublicPath);
 app.get('/portfolio',function (_, res) { res.sendFile(portfolioIndexPath); });
 
 //Setup Example File
-const exampleIndexPath = path.join(__dirname, '/portfolio/example/example.csv');
-const examplePublicPath = express.static(path.join(__dirname, '/portfolio/example'));
-app.use('/portfolio/example', examplePublicPath);
-app.get('/portfolio/example',function (_, res) { res.sendFile(exampleIndexPath); });
+const exampleCsvPath = path.join(__dirname, '/portfolio/example/example.csv');
+app.use('/example', exampleCsvPath);
 
 //Setup ConcertDb
 const concertDbIndexPath = path.join(__dirname, '/public/index.html');
